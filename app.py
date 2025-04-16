394f62589652597dfab77392647a9caf6ac7df79
Vicentevv <116390036+Vicentevv@users.noreply.github.com>
2025-04-16 09:34:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
-from datetime import datetime
+import requests
 
-app = Flask(__name__)
+url = "https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt"
+response = requests.get(url)
 
-@app.route('/')
-def home():
-    actual = datetime.now()
-    fecha_formateada = actual.strftime("%d, %B, %Y, %M, %H, %S")
-    return f'¡Hola, LOJA capital cultural! <b>{fecha_formateada}</b>'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if response.status_code == 200:
+    print("Contenido descargado:")
+    print(response.text)
+else:
+    print(f"Error al acceder a la URL: {response.status_code}")
